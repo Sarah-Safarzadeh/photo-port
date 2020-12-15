5c98d72584d73e8baeeb5fda7e3d881c70a8d736
Sarah-Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2020-12-15 14:07:01 -0600

    nav and About component functions and impor exports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './components/About';
+import './components/Nav';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
