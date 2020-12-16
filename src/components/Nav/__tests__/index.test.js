4b0641ad057ee066fbe95c38754ff14bc839f58a
Sarah-Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2020-12-15 20:26:08 -0600

    wrote tests for links and emoji visibilibty

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -16,3 +16,17 @@ describe('Nav component', () => {
         expect(asFragment()).toMatchSnapshot();
     });
 });
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+  })
+
+  describe('links are visible', () => {
+    test('inserts text into the links', () => {
+
+    });
+  })
