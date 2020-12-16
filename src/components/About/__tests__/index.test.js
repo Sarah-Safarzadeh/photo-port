91e2e7b5365c70a089eef8e6dd07cb41f3e9e43f
Sarah-Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2020-12-15 19:45:59 -0600

    tests written for baout

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,3 +3,17 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+      render(<About />);
+    });
+  
+    // Second Test
+    test('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+  })
