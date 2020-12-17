4e4b32fa263e73056029568d46723bfbba309b16
Sarah-Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2020-12-17 16:50:20 -0600

    added contact component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import PhotoList from './components/PhotoList';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
